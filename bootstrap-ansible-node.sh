86805092ff4520315475ab1b4372753c413b5963
William Smith <pastorofmuppets@me.com>
2024-06-26 13:13:19 -0700

    added bootstrap-ansible-node.sh

---
+++ bootstrap-ansible-node.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+mkdir -p /usr/local/etc/pkg/repos
+cat << EOF >> /usr/local/etc/pkg/repos/FreeBSD.conf
+FreeBSD { enabled: no }
+FreeBSD-ports {
+  url: "pkg+https://pkg.FreeBSD.org/${ABI}/latest",
+  mirror_type: "srv",
+  signature_type: "fingerprints",
+  fingerprints: "/usr/share/keys/pkg",
+  enabled: yes
+}
+EOF
+
+pkg bootstrap -y
+pkg install -y python
